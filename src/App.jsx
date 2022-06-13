de9ed6063dc1d2a8278768b41b0d0922a8b5afbd
koendebruijn <bruijnk@gmail.com>
2022-06-13 20:36:52 +0200

    Added references

---
+++ src/App.jsx
@@ -0,0 +1,100 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className='App'>
+      <div id='bibliography' className='bibliography'>
+        <div className='references'>
+          <h1>References</h1>
+        </div>
+        <p className='reference'>
+          Den Haag. (n.d.). <i>Mobiliteit</i>. Denhaag.incijfers.nl. Retrieved
+          April 16, 2022, from{" "}
+          <a
+            href='https://denhaag.incijfers.nl/dashboard/Overzichten/Mobiliteit/'
+            rel='nofollow'
+            target='blank'
+          >
+            https://denhaag.incijfers.nl/dashboard/Overzichten/Mobiliteit/
+          </a>
+        </p>
+        <p className='reference'>
+          Den Haag. (2011).
+          <i>
+            Klimaatplan Den Haag Bijlage 1 De Haagse klimaatopgave in cijfers{" "}
+          </i>
+          .
+          <a
+            href='https://denhaag.raadsinformatie.nl/document/3350182/1/RIS180174a'
+            rel='nofollow'
+            target='blank'
+          >
+            https://denhaag.raadsinformatie.nl/document/3350182/1/RIS180174a
+          </a>
+        </p>
+        <p className='reference'>
+          European Commission. (n.d.).
+          <i>
+            CO<sub>2</sub> emission performance standards for cars and vans
+          </i>
+          . Ec.europa.eu.{" "}
+          <a
+            href='https://ec.europa.eu/clima/eu-action/transport-emissions/road-transport-reducing-co2-emissions-vehicles/co2-emission-performance-standards-cars-and-vans_en'
+            rel='nofollow'
+            target='blank'
+          >
+            https://ec.europa.eu/clima/eu-action/transport-emissions/road-transport-reducing-co2-emissions-vehicles/co2-emission-performance-standards-cars-and-vans_en
+          </a>
+        </p>
+        <p className='reference'>
+          Google. (n.d.). <i>Traffic Layer | Maps JavaScript API</i>. Google
+          Developers. Retrieved April 16, 2022, from{" "}
+          <a
+            href='https://developers.google.com/maps/documentation/javascript/examples/layer-traffic'
+            rel='nofollow'
+            target='blank'
+          >
+            https://developers.google.com/maps/documentation/javascript/examples/layer-traffic
+          </a>
+        </p>
+        <p className='reference'>
+          Ritchie, H., &amp; Roser, M. (n.d.). <i>Emissions by sector</i>. Our
+          World in Data.{" "}
+          <a
+            href='https://ourworldindata.org/emissions-by-sector#energy-electricity-heat-and-transport-73-2'
+            rel='nofollow'
+            target='blank'
+          >
+            https://ourworldindata.org/emissions-by-sector#energy-electricity-heat-and-transport-73-2
+          </a>
+        </p>
+        <p className='reference'>
+          <i>Travel carbon footprint by transport mode</i>. (n.d.). Statista.{" "}
+          <a
+            href='https://www.statista.com/statistics/1185559/carbon-footprint-of-travel-per-kilometer-by-mode-of-transport/'
+            rel='nofollow'
+            target='blank'
+          >
+            https://www.statista.com/statistics/1185559/carbon-footprint-of-travel-per-kilometer-by-mode-of-transport/
+          </a>
+        </p>
+        <p className='reference'>
+          US EPA. (2018, May 10).
+          <i>
+            Greenhouse Gas Emissions from a Typical Passenger Vehicle | US EPA
+          </i>
+          . US EPA; US EPA.{" "}
+          <a
+            href='https://www.epa.gov/greenvehicles/greenhouse-gas-emissions-typical-passenger-vehicle'
+            rel='nofollow'
+            target='blank'
+          >
+            https://www.epa.gov/greenvehicles/greenhouse-gas-emissions-typical-passenger-vehicle
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
